0649dc9d429e84a201213c3c8d81fcfce0cbf264
Peter Klingelhofer <peterklingelhofer@gmail.com>
2023-01-22 23:00:39 -0600

    remove tempo parameter

--- index.py
+++ index.py
@@ -3,7 +3,6 @@ from pydub import AudioSegment
 from pydub.generators import Sine
 from tqdm import tqdm
 
-tempo = int(input("Enter the desired tempo (in beat per minute): "))
 duration = int(input("Enter the desired duration (in seconds): ")) * 1000
 
 image = Image.open("image.png")
@@ -11,7 +10,7 @@ pixels = image.load()
 
 audio = AudioSegment.silent(duration=duration)
 
-pbar = tqdm(total=image.width*image.height)
+pbar = tqdm(total=image.width * image.height)
 
 for x in range(image.width):
     for y in range(image.height):
@@ -19,14 +18,12 @@ for x in range(image.width):
 
         frequency = color[0] + color[1] + color[2]
 
-        sine_wave = Sine(frequency).to_audio_segment(duration=50)
+        sineWave = Sine(frequency).to_audio_segment(duration=50)
 
-        audio = audio + sine_wave
+        audio = audio + sineWave
 
         pbar.update(1)
 
 pbar.close()
-
-audio = audio.speed_change(tempo / audio.frame_rate)
 
 audio.export("music.wav", format="wav")
